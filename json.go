74a7f7d56a27e684f50a17426aa03597c30268ec
x <you@example.com>
2022-10-08 16:13:02 +0800

    chore(json): update JSONIndent

--- json.go
+++ json.go
@@ -1,31 +1,25 @@
 package goutils
 
 import (
-	"bytes"
 	"encoding/json"
 )
 
 // JSONIndent converts the golang value to indent JSON string
 // such as a struct, map, slice, array and so on
 func JSONIndent(obj interface{}) string {
-	bs, err := json.Marshal(obj)
+	buf, err := json.MarshalIndent(obj, "", "	")
 	if err != nil {
 		return err.Error()
 	}
-	buf := new(bytes.Buffer)
-	err = json.Indent(buf, bs, "", "	")
-	if err != nil {
-		return err.Error()
-	}
-	return buf.String()
+	return string(buf)
 }
 
 // JSONString converts the golang value to JSON string
 // such as a struct, map, slice, array and so on
 func JSONString(obj interface{}) string {
-	bs, err := json.Marshal(obj)
+	buf, err := json.Marshal(obj)
 	if err != nil {
 		return err.Error()
 	}
-	return string(bs)
+	return string(buf)
 }
